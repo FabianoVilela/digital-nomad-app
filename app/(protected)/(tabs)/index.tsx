9a626bfc7db171bbd2ab8814381189050f36be5c
Fabiano Vilela <fabiano.vilela@gmail.com>
2025-06-25 21:02:21 -0300

    refactor: simplify layout and remove unused components

--- app/(protected)/(tabs)/index.tsx
+++ app/(protected)/(tabs)/index.tsx
@@ -1,40 +1,11 @@
-import { Image } from 'expo-image';
 import { Link } from 'expo-router';
-import { StyleSheet, Text } from 'react-native';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
+import { Text, View } from 'react-native';
 
 export default function HomeScreen() {
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }
-    >
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Text>Home</Text>
       <Link href="/(protected)/city-details/1">City details</Link>
-    </ParallaxScrollView>
+    </View>
   );
 }
-
-const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
-});
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,17 +1,9 @@
-import {
-  DarkTheme,
-  DefaultTheme,
-  ThemeProvider,
-} from '@react-navigation/native';
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
 
-import { useColorScheme } from '@/hooks/useColorScheme';
-
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
   const [loaded] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
   });
@@ -22,13 +14,11 @@ export default function RootLayout() {
   }
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="(protected)" />
-        <Stack.Screen name="(public)/auth/sign-in" />
-        <Stack.Screen name="+not-found" />
-      </Stack>
+    <Stack screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="(protected)" />
+      <Stack.Screen name="(public)/auth/sign-in" />
+      <Stack.Screen name="+not-found" />
       <StatusBar style="auto" />
-    </ThemeProvider>
+    </Stack>
   );
 }
