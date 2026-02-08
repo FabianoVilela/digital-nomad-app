e4d21e51739c2bdd1905c49a6341eda5270b9f4a
Fabiano Vilela <fabiano.vilela@gmail.com>
2026-02-08 11:02:49 -0300

    feat: add useDebounce hook

---
+++ src/hooks/useDebounce.ts
@@ -0,0 +1,20 @@
+import { useEffect, useState } from 'react';
+
+interface UseDebounceProps<T> {
+  value: T;
+  delay?: number;
+}
+
+export function useDebounce<T>({ value, delay = 500 }: UseDebounceProps<T>) {
+  const [debouncedValue, setDebouncedValue] = useState<T>(value);
+
+  useEffect(() => {
+    const handler = setTimeout(() => {
+      setDebouncedValue(value);
+    }, delay);
+
+    return () => clearTimeout(handler);
+  }, [value, delay]);
+
+  return debouncedValue;
+}
