c6a354e2bd11fe11c96f85f8d88939c2ff2f8cfb
Fabiano Vilela <fabiano.vilela@gmail.com>
2026-03-20 12:05:19 -0300

    feat: Add SafeArea Screen component

--- src/ui/components/base/Screen.tsx
+++ src/ui/components/base/Screen.tsx
@@ -1,20 +1,23 @@
 import type { PropsWithChildren } from 'react';
 import { ScrollView } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 import { useAppTheme } from '@/ui/theme';
 import { Box, type BoxProps } from './Box';
 
 interface ScreenProps extends PropsWithChildren<BoxProps> {
   scrollable?: boolean;
+  safeArea?: boolean;
 }
 
 export function Screen({
   children,
   scrollable = false,
+  safeArea = false,
   ...props
 }: ScreenProps) {
   const { spacing, colors } = useAppTheme();
 
-  if (scrollable) {
+  function ScrollableContent() {
     return (
       <ScrollView
         contentContainerStyle={{
@@ -28,14 +31,38 @@ export function Screen({
     );
   }
 
-  return (
-    <Box
-      backgroundColor="background"
-      flex={1}
-      paddingHorizontal="s16"
-      {...props}
-    >
-      {children}
-    </Box>
-  );
+  function NonScrollableContent() {
+    return (
+      <Box
+        backgroundColor="background"
+        flex={1}
+        paddingHorizontal="s16"
+        {...props}
+      >
+        {children}
+      </Box>
+    );
+  }
+
+  if (scrollable) {
+    if (safeArea) {
+      return (
+        <SafeAreaView style={{ flex: 1, backgroundColor: colors.background }}>
+          <ScrollableContent />
+        </SafeAreaView>
+      );
+    }
+
+    return <ScrollableContent />;
+  }
+
+  if (safeArea) {
+    return (
+      <SafeAreaView style={{ flex: 1, backgroundColor: colors.background }}>
+        <NonScrollableContent />
+      </SafeAreaView>
+    );
+  }
+
+  return <NonScrollableContent />;
 }
